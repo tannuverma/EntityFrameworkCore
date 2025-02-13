1cbbf9ab39996edfaa754db8d20babd31a113bba
tannuverma <tannu.verma@nagarro.com>
2025-02-14 02:22:04 +0530

    Added few more methods in CurrencyContoller

--- DbOperationsEFCore/DbOperationsEFCore/Controllers/CurrencyController.cs
+++ DbOperationsEFCore/DbOperationsEFCore/Controllers/CurrencyController.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DbOperationsEFCore.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/currencies")]
     [ApiController]
     public class CurrencyController : ControllerBase
     {
@@ -26,6 +26,29 @@ namespace DbOperationsEFCore.Controllers
 
             //method 2(SQl query format)
             var result = await (from currencies in _appDbContext.Currencies select currencies).ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCurrencyByIdAsync([FromRoute] int id)
+        {
+            var result = await _appDbContext.Currencies.FindAsync(id);
+            return Ok(result);
+        }
+
+        //[HttpGet("{name}")]
+        //public async Task<IActionResult> GetCurrencyByIdAsync([FromRoute] string name)
+        //{
+        //    var result = await _appDbContext.Currencies.FirstOrDefaultAsync(x => x.Title == name);
+        //    return Ok(result);
+        //}
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetCurrencyByIdAsync([FromRoute] string name, [FromQuery] string? description)
+        {
+            var result = await _appDbContext.Currencies
+                .FirstOrDefaultAsync(x => x.Title == name 
+                && (string.IsNullOrEmpty(description) || x.Description == description));
             return Ok(result);
         }
     }
